87f82509974ebc7016992b74e1c881e112b0c851
Nazli-Madatova <158826613+Nazli-Madatova@users.noreply.github.com>
2024-02-25 10:27:16 +0400

    Add files via upload

---
+++ Hw3.py
@@ -0,0 +1,19 @@
+result = []
+def divider(a, b):
+        if a < b:
+            raise ValueError
+        if b > 100:
+            raise IndexError
+        return a/b
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+
+for key in data:
+
+    try:
+        res = divider(key, data[kem])
+        result.append(res)
+    except:
+        print("Error. Wrong data name.")
+
+print(result)
